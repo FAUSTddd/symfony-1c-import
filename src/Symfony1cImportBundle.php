d670c8c5c9da1b1582bdf8f64c0bc055207ef83b
faustddd <faustddd@yandex.ru>
2025-12-23 17:47:45 +0300

    fix: override getContainerExtension() to match Bundle name

--- src/Symfony1cImportBundle.php
+++ src/Symfony1cImportBundle.php
@@ -1,10 +1,19 @@
 <?php
-
+// src/Symfony1cImportBundle.php
 namespace FaustDDD\Symfony1cImport;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use FaustDDD\Symfony1cImport\DependencyInjection\Symfony1cImportExtension;
 
 final class Symfony1cImportBundle extends Bundle
 {
-    // пусто, но это нормально
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new Symfony1cImportExtension();
+        }
+
+        return $this->extension;
+    }
 }
