dd4b30974303813935e48008dd0c2def5e0d681b
faustddd <faustddd@yandex.ru>
2025-12-23 19:59:10 +0300

    fix: route

--- config/services.php
+++ config/services.php
@@ -1,5 +1,4 @@
 <?php
-// config/services.php  (внутри вашего бандла)
 declare(strict_types=1);
 
 use FaustDDD\Symfony1cImport\Infrastructure\Controller\Import1CController;
@@ -8,32 +7,27 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return function (ContainerConfigurator $container): void {
-    // служебные параметры
-    $container->parameters()
-        ->set('import_1c_login', '%env(IMPORT_1C_LOGIN)%')
-        ->set('import_1c_password', '%env(IMPORT_1C_PASSWORD)%');
-
     $services = $container->services()
         ->defaults()
         ->autowire()
         ->autoconfigure();
 
-    // авто-регистрация всего пакета
-    $services->load('FaustDDD\\Symfony1cImport\\', __DIR__.'/../src/')
-        ->exclude([
-            __DIR__.'/../src/Domain/',
-            __DIR__.'/../src/Application/Command/',
-        ]);
-
-    // явная регистрация контроллера (чтобы AbstractController не ругался)
+    // 1. Регистрируем контроллер ПЕРВЫМ и явно
     $services->set(Import1CController::class)
         ->public()
         ->tag('controller.service_arguments')
         ->arg('$interactor', service(Cml1cInteractor::class));
 
-    // ручная настройка интерактора
+    // 2. Остальные сервисы
+    $services->load('FaustDDD\\Symfony1cImport\\', __DIR__.'/../src/')
+        ->exclude([
+            __DIR__.'/../src/Domain/',
+            __DIR__.'/../src/Application/Command/',
+            __DIR__.'/../src/Infrastructure/Controller/', // <-- исключаем, чтобы не дублировать
+        ]);
+
     $services->set(Cml1cInteractor::class)
         ->arg('$projectDir', '%kernel.project_dir%')
-        ->arg('$login', '%import_1c_login%')
-        ->arg('$password', '%import_1c_password%');
+        ->arg('$login', '%env(IMPORT_1C_LOGIN)%')
+        ->arg('$password', '%env(IMPORT_1C_PASSWORD)%');
 };
